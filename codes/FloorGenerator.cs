b357749eddcecd6fd78837ddce818f053714241b
Uranuss <majorsaint@naver.com>
2015-03-03 00:04:25 +0900

    지형 생성기
    추가 및 테스트

---
+++ codes/FloorGenerator.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorGenerator : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject baseGround = null;
+	// Use this for initialization
+	private GameObject [] floorInstance = new GameObject[10];
+	void Start () {
+		for(int i = 0; i < 10; ++i)
+		{
+			floorInstance[i] = (GameObject)Instantiate(baseGround);
+			floorInstance[i].SetActive(true);
+			floorInstance[i].transform.SetParent(transform);
+			floorInstance[i].transform.position = new Vector3(i*5 - 20, 0, 0);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(GameParameters.isPause)
+			return;
+
+		for(int i = 0; i < 10; ++i)
+		{
+			floorInstance[i].transform.Translate(GameParameters.MoveSpeed*Time.deltaTime, 0, 0);
+			if(floorInstance[i].transform.position.x <= -25.0f)
+			{
+				floorInstance[i].transform.Translate(-50.0f, 0, 0);
+			}
+		}
+	}
+}
