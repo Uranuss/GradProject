a75b06e31c2ed9d0609aa227b7affb43f0899ee9
Uranuss <majorsaint@naver.com>
2015-04-29 22:15:54 +0900

    적과의 충돌체크를 확인하기 위한 더미
    테스트중

---
+++ codes/DummyObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DummyObject : MonoBehaviour {
+	void Awake() 
+	{
+	}
+
+	// Use this for initialization
+	void Start () {
+        gameObject.SetActive(false);
+        Destroy (gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
